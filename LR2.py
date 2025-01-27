55fe5a3a5d8a341c64d1362c69105c96c33f83db
Belousov-Kirill-1ISP21 <belousov.1isp21@mail.ru>
2025-01-27 11:57:55 +0300

    Add files via upload

--- LR2.py
+++ LR2.py
@@ -2,165 +2,165 @@ class Person:
 
 #Constructor
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self.__name = name
+        self.__age = age
 
 #Getters
 
     def getName(self):
-        return self.name
+        return self.__name
 
     def getAge(self):
-        return self.age
+        return self.__age
 
 #Setters
 
     def setName(self, name):
-        self.name = name
+        self.__name = name
 
     def setAge(self, age):
-        self.age = age
+        self.__age = age
 
 #DisplayInfo
     def DisplayName(self):
-        print("Имя человека:" + self.name)
+        print("Имя человека:" + self.__name)
 
     def DisplayAge(self):
-        print("Возраст человека:" + self.age)
+        print("Возраст человека:" + self.__age)
 
     def DisplayAll(self):
-        print(self.name)
-        print(self.age)
+        print(self.__name)
+        print(self.__age)
 class Customer(Person):
 
 # Constructor
     def __init__(self, name, age, visitTime):
         super().__init__(name, age)
-        self.visitTime = visitTime
+        self.__visitTime = visitTime
 
 #Getters
 
     def getVisitTime(self):
-        return self.visitTime
+        return self.__visitTime
 
 #Setters
     def setVisitTime(self, visitTime):
-        self.visitTime = visitTime
+        self.__visitTime = visitTime
 
 #DisplayInfo
 
     def DisplayVisitTime(self):
-        print("Время посещения клиентом зала:" + self.visitTime)
+        print("Время посещения клиентом зала:" + self.__visitTime)
 
     def DisplayAll(self):
-        print("Имя клиента:" + self.name)
-        print("Возраст клиента:" + self.age)
-        print("Время посещения клиентом зала:" +self.visitTime)
+        print("Имя клиента:" + self.__name)
+        print("Возраст клиента:" + self.__age)
+        print("Время посещения клиентом зала:" +self.__visitTime)
 
 class Employee(Person):
 
 # Constructor
     def __init__(self, name, age, expirienceAge):
         super().__init__(name, age)
-        self.expirienceAge = expirienceAge
+        self.__expirienceAge = expirienceAge
 
 #Getters
     def getExpirienceAge(self):
-        return self.expirienceAge
+        return self.__expirienceAge
 
 #Setters
     def setExpirienceAge(self, expirienceAge):
-        self.expirienceAge = expirienceAge
+        self.__expirienceAge = expirienceAge
 
 #DisplayInfo
     def DisplayExpirienceAge(self):
-        print("Стаж сотрудника:" + self.expirienceAge)
+        print("Стаж сотрудника:" + self.__expirienceAge)
 
     def DisplayAll(self):
-        print("Имя сотрудника:" + self.name)
-        print("Возраст сотрудника:" + self.age)
-        print("Стаж сотрудника:" + self.expirienceAge)
+        print("Имя сотрудника:" + self.__name)
+        print("Возраст сотрудника:" + self.__age)
+        print("Стаж сотрудника:" + self.__expirienceAge)
 
 class Trainer(Employee):
 
 # Constructor
     def __init__(self, name, age, expirienceAge, specialisation):
         super().__init__(name, age, expirienceAge)
-        self.specialisation = specialisation
+        self.__specialisation = specialisation
 
 #Getters
     def getSpecialisation(self):
-        return self.specialisation
+        return self.__specialisation
 
 #Setters
     def setSpecialisation(self, specialisation):
-        self.specialisation = specialisation
+        self.__specialisation = specialisation
 
 
 # DisplayInfo
     def DisplaySpecialisation(self):
-        print("Специализация тренера:" + self.specialisation)
+        print("Специализация тренера:" + self.__specialisation)
 
     def DisplayAll(self):
-        print("Имя тренера:" + self.name)
-        print("Возраст тренера:" + self.age)
-        print("Стаж тренера:" + self.expirienceAge)
-        print("Специализация тренера:" + self.specialisation)
+        print("Имя тренера:" + self.__name)
+        print("Возраст тренера:" + self.__age)
+        print("Стаж тренера:" + self.__expirienceAge)
+        print("Специализация тренера:" + self.__specialisation)
 
 class Training:
 
 # Constructor
     def __init__(self, time, trainer,customer,duration):
-        self.time = time
-        self.trainer = trainer
-        self.customer = customer
-        self.duration = duration
+        self.__time = time
+        self.__trainer = trainer
+        self.__customer = customer
+        self.__duration = duration
 
 #Getters
     def getTime(self):
-        return self.time
+        return self.__time
 
     def getTrainer(self):
-        return self.trainer
+        return self.__trainer
 
     def getCustomer(self):
-        return self.customer
+        return self.__customer
 
     def getDuration(self):
-        return self.duration
+        return self.__duration
 
 #Setters
     def setTrainer(self, trainer):
-        self.trainer = trainer
+        self.__trainer = trainer
 
     def setCustomer(self, customer):
-        self.customer = customer
+        self.__customer = customer
 
     def setDuration(self, duration):
-        self.duration = duration
+        self.__duration = duration
 
     def setTime(self, time):
-        self.time = time
+        self.__time = time
 
 #DisplayInfo
 
     def DisplayTrainer(self):
-        print("Тренер тренировки:" + self.trainer)
+        print("Тренер тренировки:" + self.__trainer)
 
     def DisplayCustomer(self):
-        print("Клиент тренировки:" + self.customer)
+        print("Клиент тренировки:" + self.__customer)
 
     def DisplayDuration(self):
-        print("Продолжителньость тренировки:" + self.duration)
+        print("Продолжителньость тренировки:" + self.__duration)
 
     def DisplayTime(self):
-        print("Время начала тренировки:" + self.time)
+        print("Время начала тренировки:" + self.__time)
 
     def DisplayAll(self):
-        print("Тренер тренировки:" + self.trainer.getName())
-        print("Клиент тренировки:" + self.customer.getName())
-        print("Продолжителньость тренировки:" + str(self.duration))
-        print("Время начала тренировки:" + str(self.time))
+        print("Тренер тренировки:" + self.__trainer.getName())
+        print("Клиент тренировки:" + self.__customer.getName())
+        print("Продолжителньость тренировки:" + str(self.__duration))
+        print("Время начала тренировки:" + str(self.__time))
 
 
 client1 = Customer("Ivan", 35, 12)
@@ -180,5 +180,3 @@ for i in range(len(trainingList)):
 
 for i in range(len(clientList)):
     print("Для клиента " + clientList[i].getName() + " тренером является " + clientsTrainerList.get(clientList[i]).getName() + ".")
-
-
